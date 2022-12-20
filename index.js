2bc460cca670b3fdbe70c0c11ae9b16254b3cf8e
Enzofromspace <8026658+Enzofromspace@users.noreply.github.com>
2022-12-20 13:37:19 +0300

    Set the API request to get joke object in JSON

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Import stylesheets
 import './style.css';
+const jokeButton = document.querySelector('.getJoke');
+const jokeHolder = document.querySelector('.Joke p');
 
 // Write Javascript code!
 const buttonText = [
@@ -12,3 +14,15 @@ const buttonText = [
   'please stop',
   'that was the worst one',
 ];
+
+async function fetchJoke(){
+  const response = await fetch('https://icanhazdadjoke.com', {
+    headers:{
+      Accept: 'application/json',
+    }
+  });
+  const data = await response.json();
+  return data;
+}
+ 
+fetchJoke();
