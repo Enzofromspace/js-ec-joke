439e1a8a599c1f7c017c5262aed3650de88af9ab
Enzofromspace <8026658+Enzofromspace@users.noreply.github.com>
2022-12-20 14:16:17 +0300

    Got CSS loader to add then remove

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 import './style.css';
 const jokeButton = document.querySelector('.getJoke');
 const jokeHolder = document.querySelector('.joke p');
+const loader = document.querySelector('.loader');
 
 // Write Javascript code!
 const buttonText = [
@@ -14,17 +15,23 @@ const buttonText = [
   'please stop',
   'that was the worst one',
 ];
-
+// call API
 async function fetchJoke(){
+  // turn on loader CSS animation
+  loader.classList.remove('hidden');
+  //remove joke button while loading
+  jokeButton.classList.add('hidden');
   const response = await fetch('https://icanhazdadjoke.com', {
     headers:{
       Accept: 'application/json',
     }
   });
   const data = await response.json();
+  loader.classList.add('hidden');
+  jokeButton.classList.remove('hidden');
   return data;
 }
-
+// Loop over options in buttonText object
 function randomItemFromArray(arr, not){
   const item = arr[Math.floor(Math.random() * arr.length)];
   if(item === not){
@@ -32,12 +39,12 @@ function randomItemFromArray(arr, not){
   }
   return item;
 }
-
+//What happens when HTML +button is cliked
 async function handleClick(){
   const { joke } = await fetchJoke();
   jokeHolder.textContent = joke;
   jokeButton.textContent = randomItemFromArray(buttonText, jokeButton.textContent);
 }
-
+//event for button click to call other functions
 jokeButton.addEventListener('click', handleClick);
-fetchJoke();
+fetchJoke();  
